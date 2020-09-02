f47f49ac99ec5f2c34504c34ece374e43403902d
Maksim Lebedev <firya@bk.ru>
2020-09-02 16:39:06 +0300

    v 1.2

--- myscript.js
+++ myscript.js
@@ -1,15 +1,28 @@
 var prevContentHeight = null;
+const TEXT = ' — геймеры в ярости';
 
 const scrollHandler = () => {
   if (document.body.scrollHeight > prevContentHeight) {
     prevContentHeight = document.body.scrollHeight;
-    var titles = document.querySelectorAll('.content-header__title')
+    var titles = document.querySelectorAll('.content-header__title');
 
     for (let i = 0; i < titles.length; i++) {
       const title = titles[i];
+
+      const wrap = title.querySelector('.l-no-wrap');
+      const checkmark = wrap ? wrap.querySelector('a') : null;
+
+      console.log(checkmark)
+
+      const textNode = document.createTextNode(TEXT);
+
       if (!title.classList.contains("is-raged")) {
         title.classList.add("is-raged");
-        title.innerHTML = `${title.innerHTML} — геймеры в ярости`;
+        if (checkmark) {
+          checkmark.parentNode.insertBefore(textNode, checkmark);
+        } else {
+          title.innerHTML = `${title.innerHTML}${TEXT}`;
+        }
       }
     }
   }
